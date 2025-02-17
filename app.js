83049d753c734bd49dcd32fa853e48476d337d16
Darshan Gummadi <darshangummadi@usf.edu>
2025-02-17 16:29:18 -0500

    Created-JSandCSSfiles

---
+++ app.js
@@ -0,0 +1,26 @@
+
+
+const parent = React.createElement("div", {id:"parent"},
+    [React.createElement("div", {id:"child1"},
+        [React.createElement("h1", {id:"heading1"}, "Hello"),
+        React.createElement("h1", {id:"heading2"}, "Hello1"),
+        React.createElement("h1", {id:"heading3"}, "Hello2"),
+        React.createElement("h1", {id:"heading4"}, "Hello3")
+    ]),
+    React.createElement("div", {id:"child2"},
+        [React.createElement("h1", {id:"heading1"}, "Hello"),
+        React.createElement("h1", {id:"heading2"}, "Hello1"),
+        React.createElement("h1", {id:"heading3"}, "Hello2"),
+        React.createElement("h1", {id:"heading4"}, "Hello3")
+    ])]
+);
+
+
+const heading = React.createElement(
+    "h1", 
+    {id:"heading", xyz:"abc"}, 
+    "Hello World from React");
+console.log(heading);
+const root = ReactDOM.createRoot(document.getElementById("root"));
+console.log(root);
+root.render(parent);
